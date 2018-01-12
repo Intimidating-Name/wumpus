5f29bf0c61297fa2764357acc7a1e7d3a8388b94
aquarium <pvashevko@gmail.com>
2018-01-12 08:34:08 -0500

    Added comments, reliable way of searching caves

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 import global_data as gd
 
 def move():
-
+    
     while True:
         response = input("Which cave do you want to move to?\n")
+        # if response is an int
         try:
             response = int(response)
         except ValueError:
             print("You can not go there! Try Again.")
             continue
+        # if response is a valid cave connection
         if response in gd.cave.connections:
-            gd.cave = gd.caves[response]
+            # search for cave with id of response
+            gd.cave = [x for x in gd.caves if x.id == response][0]
             print("You have moved to cave " + str(gd.cave.id))
             break
         else:
             print("You can not go there! Try Again.")
-
-    print(gd.cave.id)
 
 def shoot():
 
@@ -39,13 +40,19 @@ def main():
     # MATRIX_COL = 3
     # MATRIX_ROW = 3
     # matrix = [[0 for x in range(MATRIX_COL)] for x in range(MATRIX_ROW)]
+    
+    # print intro
     print(gd.intro)
-    gd.cave = gd.caves[0]
+    # set cave to 0
+    gd.cave = [x for x in gd.caves if x.id == 0][0]
+    # game function
     while True:
+        # print metadata
         print("Your are currently in cave " + str(gd.cave.id))
         print(gd.cave.desc)
         print("You can go to caves " + ", ".join([str(i) for i in gd.cave.connections]) + " from here")
         decision()
 
+# if run as main
 if __name__ == '__main__':
     main()
