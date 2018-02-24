9b65672ab626cce65e8b5899c64fdf9e6b950469
Intimidating-Name <njelbaum2@gmail.com>
2018-02-24 11:17:27 -0500

    a lot of excellent refactoring game is now smart enough to generate pit and bat without blocking player from 1 wumpus added constants

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import json
-from pprint import pprint
 import winsound
 from random import randint
 from pyfiglet import Figlet
+
+CAVE_BAT = 0
+CAVE_PIT = 1
+CAVE_DESCRIPTION = 2
+CAVE_CONNECTIONS = 3
+CAVE_SOUND = 4
 
 def initial_state():
     global caves
@@ -11,30 +16,40 @@ def initial_state():
     global playing
     global wumpus_cave
     global wumpus_alive
+    global CAVE_BAT
     caves = json.load(open("caves.json", "r"))
     current_cave = 0
     alive = True
     playing = True
-    wumpus_cave = random_cave()
+    wumpus_cave = random_wumpus_cave()
     wumpus_alive = True
+    bat_possible_count = randint(0, 3)
+    for x in range(0, randint(0, bat_possible_count)):
+        caves[random_hazzard_cave()][CAVE_BAT] = True
+    pit_possible_count = 3 - bat_possible_count
+    for x in range(0, randint(0, pit_possible_count)):
+        caves[random_hazzard_cave()][CAVE_PIT] = True
+
 
 def move(connections):
     global current_cave
     global caves
     global alive
     global wumpus_cave
+    global CAVE_CONNECTIONS
+    global CAVE_BAT
     move_to = int(input("Which cave do you want to move to?"))
-    winsound.PlaySound(caves[move_to][5], winsound.SND_FILENAME | winsound.SND_ASYNC)
+    winsound.PlaySound(caves[move_to][CAVE_SOUND], winsound.SND_FILENAME | winsound.SND_ASYNC)
     if move_to in connections:
-        print("you  moved to cave " + str(move_to))
+        print("you moved to cave " + str(move_to))
         current_cave = move_to
-        if caves[current_cave][1]:
+        if caves[current_cave][CAVE_PIT]:
             print("You fell into the bottomless pit.")
             alive = False
-        if caves[current_cave][2]:
+        if move_to is wumpus_cave:
             print("Before you die, you think you hear the wumpus burp. What bad manners it has.")
             alive = False
-        if caves[current_cave][0]:
+        if caves[current_cave][CAVE_BAT]:
             print("This cave has bats. You are now going to be carried to a random, safe cave.")
             current_cave = random_cave()
     else:
@@ -43,16 +58,19 @@ def move(connections):
 
 def shoot(connections, caves):
     global wumpus_alive
+    global wumpus_cave
+    global CAVE_BAT
+    global CAVE_CONNECTIONS
     shoot_to = int(input("Which cave do you want to shoot to?"))
     if shoot_to not in connections:
         print("You cannot shoot there. Try again")
-        shoot(caves[current_cave][4], caves)
-    if caves[wumpus_cave][2]:
+        shoot(caves[current_cave][CAVE_CONNECTIONS], caves)
+    if shoot_to is wumpus_cave:
         print("You shot the wumpus! You quickly escape and win.")
         wumpus_alive = False
-    if caves[shoot_to][0]:
+    if caves[shoot_to][CAVE_BAT]:
         print("You shot a bat and feel really bad. You promise to plan the funeral as soon as you kill the wumpus.")
-        caves[shoot_to][0] = False
+        caves[shoot_to][CAVE_BAT] = False
 
 def random_tunnel(connections):
     chosen = randint(0, 3)
@@ -62,29 +80,51 @@ def random_tunnel(connections):
 
 
 
-def random_cave():
+def random_dropoff_cave():
     global wumpus_cave
-    safeRandomCaveNumber = randint(0, 24)
-    while wumpus_cave == safeRandomCaveNumber or 8 == safeRandomCaveNumber or 1 == safeRandomCaveNumber or 15 == safeRandomCaveNumber:
-        safeRandomCaveNumber = randint(0, 24)
-    return safeRandomCaveNumber
+    global caves
+    global CAVE_BAT
+    global CAVE_PIT
+    safe_random_cave = randint(0, 24)
+    while wumpus_cave is safe_random_cave or caves[safe_random_cave][CAVE_BAT] or caves[safe_random_cave][CAVE_PIT]:
+        safe_random_cave = randint(0, 24)
+    return safe_random_cave
+
+def random_wumpus_cave():
+    return randint(1, 24)
+
+def random_hazzard_cave():
+    global wumpus_cave
+    global caves
+    global CAVE_BAT
+    global CAVE_PIT
+    choice = randint(1, 24)
+    while choice is wumpus_cave or caves[choice][CAVE_BAT] or caves[choice][CAVE_PIT]:
+        choice = randint(1, 24)
+    return choice
 
 def warnings(connections, caves):
-    for caveNum in connections:
-        if caves[caveNum][0]:
+    global wumpus_cave
+    global CAVE_BAT
+    global CAVE_PIT
+    for cave_num in connections:
+        if caves[cave_num][CAVE_BAT]:
             print("You hear bat's wings flapping.")
-        if caves[caveNum][1]:
+        if caves[cave_num][CAVE_PIT]:
             print("You feel the pit's breeze.")
-        if caves[caveNum][2]:
+        if cave_num is wumpus_cave:
             print("You smell the wumpus.")
 
 def decision():
     global caves
     global current_cave
+    global CAVE_DESCRIPTION
+    global CAVE_CONNECTIONS
+    print("")
     print("You are currently in cave " + str(current_cave))
-    print(caves[current_cave][3])
-    warnings(caves[current_cave][4], caves)
-    print("you can go to caves " + str(caves[current_cave][4]) + " from here")
+    print(caves[current_cave][CAVE_DESCRIPTION])
+    warnings(caves[current_cave][CAVE_CONNECTIONS], caves)
+    print("you can go to caves " + str(caves[current_cave][CAVE_CONNECTIONS]) + " from here")
 
     Q = input("move or shoot? (type m for move and s for shoot)").lower()
 
@@ -92,23 +132,20 @@ def decision():
 
 
 
-        move(caves[current_cave][4])
+        move(caves[current_cave][CAVE_CONNECTIONS])
 
     elif Q == "s":
 
-        shoot(caves[current_cave][4], caves)
+        shoot(caves[current_cave][CAVE_CONNECTIONS], caves)
 
     else:
 
-        print("sorry I couldn't understand you. game over")
+        print("sorry, I couldn't understand you. try again.")
 
 
 
-#pprint(caves)
 
-#caves.append = [bat, pit, wumpus, text, [connections]]
 
-#cave 19 has wumpus
 
 #cave 8 has pit
 
@@ -125,7 +162,7 @@ LAIR OF THE INFAMOUS MAN-EATING WUMPUS.  YOU ARE EQUIPPED WITH FIVE
 BENT ARROWS, AND ALL YOUR SENSES.  THERE ARE TWENTY CAVES CONNECTED
 BY TUNNELS, AND THERE ARE TWO OTHER KINDS OF HAZARDS:
         A) PITS, WHICH ARE BOTTOMLESS, AND USUALLY FATAL TO FALL
-        INTO.  THERE ARE THREE SUCH PITS IN THE NETWORK.
+        INTO.  THERE IS ONE SUCH PIT IN THE NETWORK.
         B) SUPER-BATS, WHICH IF YOU STUMBLE INTO THEIR ROOM WILL
         PICK YOU UP AND DROP YOU IN SOME RANDOM ROOM IN THE NETWORK.
         YOU MAY SHOOT SUPER-BATS, THERE IS ONE IN EACH OF THREE OR
@@ -145,11 +182,10 @@ TREMBLE WITH FEAR WHEN HE MOVES ABOUT.  YOU CAN HEAR SUPER-BATS FROM
 ONE ROOM AWAY, AND FEEL DRAFTS (FROM BOTTOMLESS PITS) FROM ONE ROOM
 AWAY.
 TO SHOOT AN ARROW TYPE 's' INSTEAD OF A MOVE, AND THEN
-SPECIFY WHICH ROOM THE ARROW SHOULD PASS THROUGH.  YOU ARE STRONG
-ENOUGH TO SHOOT IT THROUGH ONE ROOM. IF YOU
+SPECIFY WHICH ROOM THE ARROW SHOULD PASS THROUGH. YOU CAN ONLY SHOOT INTO THE NEXT ROOM.
 SPECIFY AN IMPOSSIBLE PATH THE ARROW WILL RICOCHET OFF THE WALLS OF
 THE ROOM, LOSING SPEED, AND WILL EVENTUALLY COME TO REST IN ONE OF
-THE ADJOINING ROOMS.  THE PATH MAY BE TERMINATED BY SPECIFYING ROOM 0.
+THE ADJOINING ROOMS.
 EACH ROOM IS CONNECTED TO FOUR OTHER ROOMS BY FOUR TUNNELS.  
 YOU MUST ALWAYS MOVE BETWEEN ROOMS BY SPECIFYING WHICH
 TUNNEL YOU WISH TO EXPLORE.  YOU CAN ALWAYS RETRACE YOUR FOOT STEPS
@@ -164,12 +200,12 @@ wumpus_cave = None
 wumpus_alive = None
 
 initial_state()
-print(alive, playing, current_cave, wumpus_cave, caves[wumpus_cave][2])
+print(alive, playing, current_cave, wumpus_cave, wumpus_alive)
 
-while alive and playing and caves[wumpus_cave][2]:
+while alive and playing and wumpus_alive:
 
     decision()
-    if alive == False or caves[wumpus_cave][2] == False:
+    if alive == False or wumpus_alive == False:
        play_again = input("Do you want to play again?")
        if play_again.lower() == "no":
            playing = False
